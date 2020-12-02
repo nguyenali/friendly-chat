129e42f0854407b4884f88b7af733d64fc0f8b3c
Ali Nguyen <alin57717@gmail.com>
2020-12-01 18:50:08 -0800

    create thought model for database collection

---
+++ models/Thought.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'You must include a reaction!',
+            maxlength: [280]
+        },
+        username: {
+            type: String,
+            required: 'You must enter a username!'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'You must enter a thought!',
+            minlength: [1],
+            maxlength: [280]
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'You must enter a username!'
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
