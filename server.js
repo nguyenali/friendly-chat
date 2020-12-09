6f18e15ac9230b6516246978dca565a2b07f5603
Ali Nguyen <alin57717@gmail.com>
2020-12-08 22:44:58 -0800

    correct error for dateformat

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(require('./routes'));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialize', {
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
 mongoose.set('debug', true);
